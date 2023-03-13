54ccf1cfb557116d24cebe3e8080f3dc6bfcfee7
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-03-14 02:43:47 +0300

    feat: add start section. webpack.

---
+++ shelter/webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, options) => {
+  
+  const isProduction = options.mode === 'production';
+
+  const config = {
+
+    mode: isProduction ? 'production' : 'development',
+
+    devtool: 'source-map',
+   
+
+    entry: './src/index.js',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin( {
+        template: './index.html'
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [ MiniCssExtractPlugin.loader , 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ]
+    },
+    devServer : {
+      compress: true,
+      port: 3000,
+    },
+  }
+  return config;
+};
